7cbd425ae27075d2605307cba449d7d806c4989f
Jonathan Robey <class4kayaker@gmail.com>
2016-10-21 18:29:49 -0700

    Add type checking to tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,6 +1,10 @@
 import pytest
 import grader_toolkit.cli
 from click.testing import CliRunner
+try:
+    import typing  # noqa: F401
+except:
+    pass  # Only currently using typing for annotations, no action necessary
 
 
 @pytest.mark.parametrize('text, out', [
@@ -11,6 +15,7 @@ from click.testing import CliRunner
     ('\n5 e6 4\n3 4 7 8', ''),
 ])
 def test_totaling(text, out):
+    # type: (typing.Text, typing.Text) -> None
     runner = CliRunner()
     result = runner.invoke(grader_toolkit.cli.cli_main,
                            ['util', 'totaling'],
