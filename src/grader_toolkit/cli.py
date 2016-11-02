4224fddf5a7235ddf524cf7410aa1bd5c9d20fce
Jonathan Robey <class4kayaker@gmail.com>
2016-11-01 17:38:14 -0700

    Modify defaults for better behavior

--- src/grader_toolkit/cli.py
+++ src/grader_toolkit/cli.py
@@ -16,7 +16,7 @@ def cli_main():
 
 
 @cli_main.group(name='gradebook')
-@click.option('--dbase', default='sqlite://')
+@click.option('--dbase', default='sqlite:///./grades.db')
 @click.pass_context
 def cli_gradebook(ctx, dbase):
     """Access to gradebook utilities"""
@@ -36,8 +36,8 @@ def cli_gb_add():
 
 def get_entries(kws):
     ret = {}
-    for kw in kws:
-        ret[kw] = click.prompt(*kws[kw])
+    for kw, prompt, t in kws:
+        ret[kw] = click.prompt(prompt, type=t)
     return ret
 
 
@@ -47,13 +47,16 @@ def cli_gb_add_students(session):
     """Add students to gradebook"""
     try:
         while True:
-            if click.getchar(echo='Done entering students? [yn]') == 'y':
+            click.echo('Done entering students? [yn]', nl=False)
+            c = click.getchar()
+            click.echo()
+            if c == 'y':
                 break
             s = Student(
                 **get_entries(
-                    {'id': ('Student ID:', int),
-                     'name': ('Name:', str),
-                     'email': ('Email:', str)}))
+                    [('id', 'Student ID:', int),
+                     ('name', 'Name:', str),
+                     ('email', 'Email:', str)]))
             session.add(s)
         session.commit()
     except:
@@ -67,12 +70,15 @@ def cli_gb_add_assignments(session, student_id, name, email):
     """Add assignments to gradebook"""
     try:
         while True:
-            if click.getchar(echo='Done entering students? [yn]') == 'y':
+            click.echo('Done entering assignments? [yn]', nl=False)
+            c = click.getchar()
+            click.echo()
+            if c == 'y':
                 break
             a = Assignment(
                 **get_entries(
-                    {'name': ('Name:', str),
-                     'full_credit': ('Full Credit:', float)}))
+                    [('name', 'Name:', str),
+                     ('full_credit', 'Full Credit:', float)]))
             session.add(a)
         session.commit()
     except:
@@ -105,6 +111,7 @@ def cli_gb_import(session, infile):
         session.add_all(out['students'])
         session.add_all(out['assignments'])
         session.add_all(out['grades'])
+        session.flush()
         session.commit()
     except:
         session.rollback()
@@ -129,6 +136,38 @@ def cli_gb_analyze_students(session, name, out):
         stList = session.query(Student).filter(Student.name.like(name)).all()
         for st in stList:
             grader_toolkit.analyze.analyze_student(st, stream=out)
+    except:
+        pass
+
+
+@cli_gradebook.group(name='list')
+def cli_gb_list():
+    """Listing tools"""
+    pass
+
+
+@cli_gb_list.command(name='students')
+@click.option('--out', type=click.File(mode='w'), default='-')
+@click.pass_obj
+def cli_gb_list_students(session, out):
+    """List students"""
+    try:
+        stList = session.query(Student).all()
+        for st in stList:
+            out.write(repr(st))
+    except:
+        pass
+
+
+@cli_gb_list.command(name='assignments')
+@click.option('--out', type=click.File(mode='w'), default='-')
+@click.pass_obj
+def cli_gb_list_assignments(session, out):
+    """List assignments"""
+    try:
+        stList = session.query(Student).all()
+        for st in stList:
+            out.write(repr(st))
     except:
         pass
 
