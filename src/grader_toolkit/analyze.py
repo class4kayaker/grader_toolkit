fffdfacc1a36ec2a54f07345fab1944120eb5250
Jonathan Robey <class4kayaker@gmail.com>
2017-03-01 11:00:00 -0800

    Improve assignment analysis

--- src/grader_toolkit/analyze.py
+++ src/grader_toolkit/analyze.py
@@ -25,6 +25,29 @@ def analyze_numeric_grades(grades, stream):
     stream.write('Percent Variance: {:.2%}\n'.format(var))
 
 
+def freq_grades(grades, stream, bins=10):
+    # type: (List[float], typing.TextIO, int) -> None
+    if bins < 0:
+        raise ValueError("Argument bins={} not valid (<0)".format(bins))
+    if bins == 0:
+        return
+    mxgrade = max(grades)
+    mngrade = min(grades)
+    gset = set(grades)
+    if len(gset) < bins:
+        stream.write("{:^21s}|{:^10s}\n".format("Grade", "Count"))
+        for grade in gset:
+            count = len([g for g in grades if g == grade])
+            stream.write("{:^21.4g}|{:^10.4g}\n".format(grade, count))
+    else:
+        binsize = (mxgrade-mngrade)/bins
+        stream.write('{:^21s}|{:^10s}\n'.format("Band", "Count"))
+        for i in range(bins):
+            count = len([g for g in grades if (i == int((g-mngrade)/binsize))])
+            stream.write("{:^10.4g}-{:^10.4g}|{:^10d}\n".format(
+                mngrade+i*binsize, mngrade+(i+1)*binsize, count))
+
+
 def analyze_student(student, stream, short=False):
     # type: (grader_toolkit.Student, typing.TextIO, bool) -> None
     stream.write('Name: {}\n'.format(student.name))
@@ -42,6 +65,7 @@ def analyze_assignment(assignment, stream, short=False):
     # type: (grader_toolkit.Student, typing.TextIO, bool) -> None
     stream.write('Assignment: {}\n'.format(assignment.name))
     analyze_numeric_grades(assignment.grades, stream)
+    freq_grades([g.grade for g in assignment.grades], stream)
     if short:
         return
     for g in assignment.grades:
