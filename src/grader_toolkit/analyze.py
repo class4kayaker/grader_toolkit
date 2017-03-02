5bba1f181ba96b6025b81ed1ea2aee85819adaeb
Jonathan Robey <class4kayaker@gmail.com>
2017-03-01 17:07:36 -0800

    Improve utils in analyze.py

--- src/grader_toolkit/analyze.py
+++ src/grader_toolkit/analyze.py
@@ -7,44 +7,48 @@ except:
     pass
 
 
+def weighted_grade_average(grades):
+    # type: (List[grader_toolkit.Grade]) -> float
+    s = sum([g.grade for g in grades])
+    full = sum([g.assignment.full_credit for g in grades])
+    return s/full
+
+
+def weighted_grade_deviation(grades):
+    # type: (List[grader_toolkit.Grade]) -> float
+    avg = weighted_grade_average(grades)
+    var = sum([g.grade**2/g.assignment.full_credit for g in grades])
+    full = sum([g.assignment.full_credit for g in grades])
+    return math.sqrt(var/full - avg**2)
+
+
 def analyze_numeric_grades(grades, stream):
     # type: (List[grader_toolkit.Grade], typing.TextIO) -> None
-    full = 0.
-    s = 0.
-    var = 0.
-    for g in grades:
-        grade = g.grade
-        credit = g.assignment.full_credit
-        perc = grade/credit
-        full += credit
-        s += grade
-        var += credit*perc**2
-    avg = s/full
-    var = math.sqrt(var/full - avg**2)
+    avg = weighted_grade_average(grades)
+    var = weighted_grade_deviation(grades)
     stream.write('Percent Average: {:.2%}\n'.format(avg))
-    stream.write('Percent Variance: {:.2%}\n'.format(var))
+    stream.write('Percent Deviation: {:.2%}\n'.format(var))
 
 
 def freq_grades(grades, stream, bins=10):
-    # type: (List[grader_toolkit.Grade], typing.TextIO, int) -> None
+    # type: (List[float], typing.TextIO, int) -> None
     if bins < 0:
         raise ValueError("Argument bins={} not valid (<0)".format(bins))
     if bins == 0:
         return
-    glist = [g.grade for g in grades]
-    mxgrade = max(glist)
-    mngrade = min(glist)
-    gset = set(glist)
+    mxgrade = max(grades)
+    mngrade = min(grades)
+    gset = set(grades)
     if len(gset) < bins:
         stream.write("{:^21s}|{:^10s}\n".format("Grade", "Count"))
         for grade in gset:
-            count = len([g for g in glist if g == grade])
+            count = len([g for g in grades if g == grade])
             stream.write("{:^21.4g}|{:^10.4g}\n".format(grade, count))
     else:
         binsize = (mxgrade-mngrade)/bins
         stream.write('{:^21s}|{:^10s}\n'.format("Band", "Count"))
         for i in range(bins):
-            count = len([g for g in glist
+            count = len([g for g in grades
                          if (mngrade+i*binsize <= g and
                              g <= mxgrade+(i+1)*binsize)])
             stream.write("{:^10.4g}-{:^10.4g}|{:^10d}\n".format(
@@ -68,7 +72,7 @@ def analyze_assignment(assignment, stream, short=False):
     # type: (grader_toolkit.Student, typing.TextIO, bool) -> None
     stream.write('Assignment: {}\n'.format(assignment.name))
     analyze_numeric_grades(assignment.grades, stream)
-    freq_grades(assignment.grades, stream)
+    freq_grades([g.grade for g in assignment.grades], stream)
     if short:
         return
     for g in sorted(assignment.grades, key=lambda g: g.student.name):
